3beb0445e48c20729c4d072d78c5c38227d41329
Khay <orjiemmanuel86@gmail.com>
2024-04-03 18:34:01 +0100

    added recoridng condition

--- lib/internal_audio_recorder_method_channel.dart
+++ lib/internal_audio_recorder_method_channel.dart
@@ -13,24 +13,27 @@ class MethodChannelInternalAudioRecorder extends InternalAudioRecorderPlatform {
 
   /// Stream controller to manage the data stream.
   late StreamController<List<double>> _dataStreamController;
-
+  bool isRecording = false;
   MethodChannelInternalAudioRecorder() {
     // Initialize the stream controller
+
     _dataStreamController = StreamController<List<double>>();
     // Listen for events from the platform side and add them to the stream
     methodChannel.setMethodCallHandler((call) async {
       if (call.method == 'onData') {
-        // Extract data from method call arguments and add it to the stream
-        List<dynamic> data = call.arguments;
-        debugPrint("CHunk: $data");
-        List<double> doubleData = data.cast<double>();
-        _dataStreamController.add(doubleData);
+        if (isRecording) {
+          // Extract data from method call arguments and add it to the stream
+          List<dynamic> data = call.arguments;
+          List<double> doubleData = data.cast<double>();
+          _dataStreamController.add(doubleData);
+        }
       }
     });
   }
   @override
   Future<String?> startCapturing(
       String outputPath, int encoding, int sampleRate, int delay) async {
+    isRecording = true;
     return await methodChannel.invokeMethod<String?>('startCapturing', {
       "outputPath": outputPath,
       "encoding": encoding,
@@ -42,6 +45,7 @@ class MethodChannelInternalAudioRecorder extends InternalAudioRecorderPlatform {
   @override
   Future<String?> stopCapturing() async {
     // Close the stream controller when capturing is stopped
+    isRecording = false;
     _dataStreamController.close();
     var responce = await methodChannel.invokeMethod('stopCapturing');
     return responce;
